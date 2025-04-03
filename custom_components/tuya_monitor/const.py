8e57debe09ff5815691a1290a0180426a7432125
alexlitwinski <38701080+alexlitwinski@users.noreply.github.com>
2025-04-03 08:45:35 -0300

    Update const.py

--- custom_components/tuya_monitor/const.py
+++ custom_components/tuya_monitor/const.py
@@ -8,3 +8,8 @@ CONF_DEVICES = "devices"
 CONF_DEVICE_ID = "device_id"
 CONF_PROPERTIES = "properties"
 CONF_SCAN_INTERVAL = "scan_interval"
+CONF_REFRESH_TOKEN = "refresh_token"
+CONF_TOKEN_EXPIRATION = "token_expiration"
+
+# Token expiration buffer (5 minutes in seconds)
+TOKEN_EXPIRY_BUFFER = 300
